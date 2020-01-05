4359748138230250f720daeb4e8f280c32953d2d
Andy Lombardi <lombardi.nd@gmail.com>
2020-01-05 17:52:03 +0100

    Tar instead of SCP
    Tar the folder and send it to ssh, uncompress and replace on destination

--- dist.py
+++ dist.py
@@ -2,6 +2,6 @@
 import subprocess
 import paths
 
-rsync_cmd = "scp -pr " + paths.build_folder + "/. " + paths.ssh + ":" + paths.path
+rsync_cmd = 'tar czfvv - '+ paths.build_folder +' | ssh ' + paths.ssh + ' "(cd ' + paths.server_path + ' && rm -rf ' + paths.build_folder + ' ; cat > build.tar.gz && tar -xzf build.tar.gz && rm build.tar.gz)"'
 
 subprocess.run(rsync_cmd, shell=True)
