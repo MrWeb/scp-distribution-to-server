46657b57630ec7aa3d9729b1ef0ad93476311ba2
Mr.Web <lombardi.nd@gmail.com>
2020-01-05 18:21:34 +0100

    Fix folder deletation

--- dist.py
+++ dist.py
@@ -2,11 +2,11 @@
 import subprocess
 
 # Configurations -----------
-build_folder='js' # name of the local build folder
+build_folder='dist' # name of the local build folder
 ssh='fl' # ssh config alias or full user@ip of the server
-server_path='~/www/demos' # Server destintation folder (where the app goes)
+server_path='~/public_html/live' # Server destintation folder (where the app goes)
 # End Configurations -----------
 
-rsync_cmd = 'tar czfvv - '+ paths.build_folder +' | ssh ' + paths.ssh + ' "(cd ' + paths.server_path + ' && rm -rf ' + paths.build_folder + ' ; cat > build.tar.gz && tar -xzf build.tar.gz && rm build.tar.gz)"'
+rsync_cmd = 'cd ' + build_folder + ' && tar czfvv - . | ssh ' + ssh + ' "(cd ' + server_path + ' && rm -rf * ; cat > build.tar.gz && tar -xzf build.tar.gz && rm build.tar.gz)"'
 
 subprocess.run(rsync_cmd, shell=True)
