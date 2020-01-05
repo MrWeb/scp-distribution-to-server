4b58616ff1e05ac1e0138438bb57ebe9a7676d08
Mr.Web <lombardi.nd@gmail.com>
2020-01-05 17:54:59 +0100

    Single file merge

--- dist.py
+++ dist.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import subprocess
-import paths
+
+# Configurations
+build_folder='js' #name of the build folder
+server_path='~/www/demos' #Server destintation (Where the app goes)
+ssh='fl' #ssh config alias or full user@ip
+# End Configurations
 
 rsync_cmd = 'tar czfvv - '+ paths.build_folder +' | ssh ' + paths.ssh + ' "(cd ' + paths.server_path + ' && rm -rf ' + paths.build_folder + ' ; cat > build.tar.gz && tar -xzf build.tar.gz && rm build.tar.gz)"'
 
